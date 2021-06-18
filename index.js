c9d5ff2d00574f435762673629de3864383e2939
angelo <aurtula1@gmail.com>
2021-06-18 15:31:47 -0400

    commit tree de0eb39303c5a836f407ac3d8af7a9b55262b058

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Your code here
+function saturdayFun(activity= "roller-skate") {
+    return `This Saturday, I want to ${activity}!`
+}
+
+const mondayWork = function(activity= "go to the office") {
+    return `This Monday, I will ${activity}.`
+}
+
+function wrapAdjective(adjective= "*") {
+    return function(word= "special") {
+        return `You are ${adjective}${word}${adjective}!`
+    }
+}
+
+let Calculator = new Object()
+Calculator.add = function(){
+    return 1+3
+}
+
+Calculator.subtract = function(){
+    return 1-3
+}
+
+Calculator.multiply = function(){
+    return 1*3
+}
+
+Calculator.divide = function() {
+    return 10/5
+}
